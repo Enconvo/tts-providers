c01ee225efc6a11af9a25b02ab203ee11c513b95
ysnows <guangjin2.xian@zhaopin.com.cn>
2024-04-12 17:00:19 +0800

    optimize

---
+++ src/elevenlabs_tts_provider.ts
@@ -0,0 +1,46 @@
+import { TTSItem, TTSOptions, TTSProviderBase } from "./tts_provider.ts";
+import axios from "axios";
+import { writeFile } from "fs";
+import { promisify } from "util";
+
+export default function main(ttsOptions: TTSOptions) {
+
+    return new EventLabsTTSProvider({ ttsOptions })
+}
+
+export class EventLabsTTSProvider extends TTSProviderBase {
+
+    protected async _speak({ text, audioFilePath }: { text: string; audioFilePath: string }): Promise<TTSItem> {
+
+        const writeFileAsync = promisify(writeFile);
+
+        const options = this.ttsOptions
+
+        const data = {
+            "text": text,
+            "model_id": options.modelName,
+            "voice_settings": {
+                "stability": 0.5,
+                "similarity_boost": 0.5
+            }
+        }
+
+        const ttsUrl = `${options.baseUrl}/text-to-speech/${options.voice}`
+
+        const response = await axios.post(ttsUrl, data, {
+            headers: {
+                'xi-api-key': `${options.api_key}`,
+                'Content-Type': 'application/json'
+            },
+            responseType: 'arraybuffer'
+        });
+
+        const buffer = Buffer.from(response.data);
+
+        await writeFileAsync(audioFilePath, buffer);
+        return {
+            path: audioFilePath,
+            text: text
+        }
+    }
+}
---
+++ src/elevenlabs_voices.ts
@@ -0,0 +1,58 @@
+import { environment } from "@enconvo/api";
+import fs from 'fs'
+
+
+async function fetch_model(apiKey: string) {
+
+    let models: any[] = []
+    try {
+        const resp = await fetch('https://api.elevenlabs.io/v1/voices', {
+            headers: {
+                'xi-api-key': `${apiKey}`,
+                'Content-Type': 'application/json'
+            }
+        })
+        let json = await resp.json()
+        return json['voices'].map((model: any) => {
+            return {
+                title: `${model['name']} - ${model.labels.gender} - ${model.labels.accent} - ${model.labels.description}- ${model.labels.age}- ${model.labels['use case']}`,
+                value: model['voice_id']
+            }
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
+
+    return models
+}
+
+export default async function main(req: Request) {
+    const { options } = await req.json()
+    const { text, api_key } = options
+
+    const modelCacheDir = environment.assetsPath + `/models`
+    if (!fs.existsSync(modelCacheDir)) {
+        fs.mkdirSync(modelCacheDir, { recursive: true })
+    }
+    const modelCachePath = `${modelCacheDir}/${environment.commandName}.json`
+
+    fs.existsSync(modelCachePath) || fs.writeFileSync(modelCachePath, '[]')
+
+    const modelContent = fs.readFileSync(modelCachePath, 'utf8')
+    let models = JSON.parse(modelContent)
+
+    try {
+        if (text === 'refresh' || models.length === 0) {
+            models = await fetch_model(api_key)
+            fs.writeFileSync(modelCachePath, JSON.stringify(models))
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+    return JSON.stringify(models)
+}
+
+
+
