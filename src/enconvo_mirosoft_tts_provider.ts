3aeb97fbb8842c220d2d8e1541f7eed68b951b7d
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2024-07-24 13:52:28 +0800

    默认设置解析优化

--- src/enconvo_mirosoft_tts_provider.ts
+++ src/enconvo_mirosoft_tts_provider.ts
@@ -6,23 +6,57 @@ export default function main(ttsOptions: TTSOptions) {
     return new EnconvoMicrosoftTTSProvider({ ttsOptions })
 
 }
-import Cache from "file-system-cache";
 import { homedir } from "os";
+import fs from "fs";
+import { MD5Util } from "./util/md5.ts";
 
-const cache = Cache({
+
+class Cache {
+    private basePath: string
+    constructor({ basePath }: { basePath: string }) {
+        this.basePath = basePath
+    }
+
+    async get(key: string, defaultValue: any = null) {
+        try {
+            const encrypedKey = MD5Util.getMd5(key)
+            const data = fs.readFileSync(`${this.basePath}/${encrypedKey}`).toString()
+            const json = JSON.parse(data)
+
+            // 过期
+            if (json.expireAt < Date.now()) {
+                return defaultValue
+            }
+            return json.value
+        } catch (e) {
+            return defaultValue
+        }
+    }
+
+    async set(key: string, value: any, ttl: number) {
+        const encrypedKey = MD5Util.getMd5(key)
+        fs.writeFileSync(`${this.basePath}/${encrypedKey}`, JSON.stringify({
+            value,
+            ttl,
+            expireAt: Date.now() + ttl * 1000
+        }))
+    }
+}
+
+
+
+const cache = new Cache({
     basePath: `${homedir}/Library/Caches/com.frostyeve.enconvo/token`, // (optional) Path where cache files are stored (default).
-    ns: "token-namespace",   // (optional) A grouping namespace for items.
-    hash: "sha1"         // (optional) A hashing algorithm used within the cache key.
 });
 
 export class EnconvoMicrosoftTTSProvider extends TTSProviderBase {
 
     async getToken(): Promise<{ token: string, region: string }> {
         // 本地缓存中寻找
-        const token = cache.getSync("azureSpeechToken", "") as string
+        const token = await cache.get("azureSpeechToken", "") as string
         if (token && token.length > 0) {
             console.log("get token from cache")
-            const region = cache.getSync("azureSpeechRegion", "") as string
+            const region = await cache.get("azureSpeechRegion", "") as string
             return { token, region }
         }
 
@@ -44,8 +78,8 @@ export class EnconvoMicrosoftTTSProvider extends TTSProviderBase {
         console.log("get token from server")
 
         // 缓存到本地,10分钟
-        cache.setSync("azureSpeechToken", data.token, 10 * 59)
-        cache.setSync("azureSpeechRegion", data.region, 10 * 60)
+        await cache.set("azureSpeechToken", data.token, 10 * 59)
+        await cache.set("azureSpeechRegion", data.region, 10 * 60)
 
         return { token: data.token, region: data.region }
     }
@@ -64,42 +98,47 @@ export class EnconvoMicrosoftTTSProvider extends TTSProviderBase {
     ttsSpeak(text: string, audioFilePath: string) {
         return new Promise(async (resolve, reject) => {
 
-            const { token, region } = await this.getToken()
-            console.log("token", token, "region", region)
+            try {
 
-            const speechConfig = SpeechConfig.fromAuthorizationToken(token, region);
-            const audioConfig = AudioConfig.fromAudioFileOutput(audioFilePath);
-            // mp3
-            speechConfig.speechSynthesisOutputFormat = SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3
+                const { token, region } = await this.getToken()
+                console.log("token", token, "region", region)
 
-            // The language of the voice that speaks.
-            speechConfig.speechSynthesisVoiceName = this.ttsOptions.voice.value;
+                const speechConfig = SpeechConfig.fromAuthorizationToken(token, region);
+                const audioConfig = AudioConfig.fromAudioFileOutput(audioFilePath);
+                // mp3
+                speechConfig.speechSynthesisOutputFormat = SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3
 
-            // Create the speech synthesizer.
-            var synthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+                // The language of the voice that speaks.
+                speechConfig.speechSynthesisVoiceName = this.ttsOptions.voice.value;
 
-            synthesizer.speakTextAsync(text,
-                function (result: any) {
-                    if (result.reason === ResultReason.SynthesizingAudioCompleted) {
+                // Create the speech synthesizer.
+                var synthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+
+                synthesizer.speakTextAsync(text,
+                    function (result: any) {
+                        if (result.reason === ResultReason.SynthesizingAudioCompleted) {
+                            synthesizer.close();
+                            synthesizer = null;
+                            resolve(null)
+                        } else {
+                            synthesizer.close();
+                            synthesizer = null;
+                            console.error("Speech synthesis canceled, " + result.errorDetails +
+                                "\nDid you set the speech resource key and region values?");
+                            reject(result.errorDetails)
+                        }
+                    },
+                    function (err) {
+                        console.trace("err - " + err);
                         synthesizer.close();
                         synthesizer = null;
-                        resolve(null)
-                    } else {
-                        synthesizer.close();
-                        synthesizer = null;
-                        console.error("Speech synthesis canceled, " + result.errorDetails +
-                            "\nDid you set the speech resource key and region values?");
-                        reject(result.errorDetails)
-                    }
-                },
-                function (err) {
-                    console.trace("err - " + err);
-                    synthesizer.close();
-                    synthesizer = null;
-                    reject(err)
-                });
+                        reject(err)
+                    });
 
-
+            } catch (e) {
+                console.error(e)
+                reject(e)
+            }
         })
     }
 }
